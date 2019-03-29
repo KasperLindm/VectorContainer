47b5cf44cb47b6757afbfe5199008b35718920c9
Kasper Lindmark <45844138+KasperLindm@users.noreply.github.com>
2019-03-29 14:16:53 +0100

    Updated functionallty of CheckCap (to be renamed) and a few smaller renames

--- MyVector.h
+++ MyVector.h
@@ -5,9 +5,6 @@ template <typename T>
 class MyVector
 {
 public:
-	T* data;
-	int size = 0;
-	int capacity = 1;
 
 	//if nothing is defined set everything to null
 	MyVector();
@@ -15,21 +12,21 @@ public:
 	//Destructor
 	~MyVector();
 
-	void PushBack(const T& value);
+	void PushBack(const T& value); //Would use small letter for all functions as begin and end has that
 
-	void Insert(const T& value, const int& index);
+	void Insert(const T& value, const int& index); //no const ref needed
 
 	void Swap(int firstIndex, int secondIndex);
 
 	void RemoveAt(int index);
 
-	void RemoveValue(const T& value);
+	void RemoveValue(const T& value); //Just remove is fine and standard
 
 	void Erase();
 
-	void CheckCap();
+	void CheckCap(); //can be const if it only does what it says it does
 
-	int GetSize();
+	const int GetSize(); //can be const and called just size()
 
 	typedef T* iterator;
 	iterator begin() { return &data[0]; }
@@ -37,21 +34,20 @@ public:
 
 	T& operator[](int index)
 	{
-		if (index > capacity)
-		{
-			throw std::out_of_range("index is out of range");
-		}
+		assert(index && "index out of range");
 		return data[index];
 	}
 
 	T operator[](int index) const
 	{
-		if (index > capacity)
-		{
-			throw std::out_of_range("index is out of range");
-		}
+		assert(index && "index out of range");
 		return data[index];
 	}
+
+private:
+	T* data;
+	int size = 0;
+	int capacity = 1;
 };
 
 template<typename T>
@@ -59,8 +55,10 @@ MyVector<T>::MyVector()
 	: data(new T[capacity])
 {}
 
+//Need copy constructor 
+
 template<typename T>
-MyVector<T>::~MyVector()
+MyVector<T>::~MyVector() //needs to delete your data or you get a mem leak
 {}
 
 //Place _value as far back as possible
@@ -76,17 +74,16 @@ void MyVector<T>::PushBack(const T& value)
 template<typename T>
 inline void MyVector<T>::Insert(const T& value, const int& index)
 {
-	assert(index < capacity);
-	CheckCap();
+	assert(index && "index out of range");
 	PushBack(value);
-	Swap(data[index], size - 1);
+	Swap(data[index], size - 1); //Insert shouldn't typically swap, would make diff function for that
 }
 
 //remove specific _value's and shift all values after back
 template<typename T>
 inline void MyVector<T>::RemoveValue(const T& value)
 {
-	T* tempVector = new T[capacity];
+	T* tempVector = new T[capacity]; //Shouldn't allocate new memory when removing something, never need to increase size for that
 	int amountToAdd = 0;
 	for (int i = 0; i < size; i++)
 	{
@@ -103,30 +100,29 @@ inline void MyVector<T>::RemoveValue(const T& value)
 template <typename T>
 void MyVector<T>::Swap(int firstIndex, int secondIndex)
 {
-	//assert(firstIndex && secondIndex && "First or second index out of range");
+	assert(firstIndex && secondIndex && "index out of range");
 	T temp = data[firstIndex];
 	data[firstIndex] = data[secondIndex];
 	data[secondIndex] = temp;
 }
 
 template <typename T>
-void MyVector<T>::RemoveAt(int index)
+void MyVector<T>::RemoveAt(int index) //Should be called swapToRemoveAt
 {
-	//assert(index && "index out of range");
-	Swap(index, size - 1);
-	size--;
+	assert(index && "index out of range");
+	Swap(index, --size);
 }
 
 //erase entire vector
 template<typename T>
 inline void MyVector<T>::Erase()
 {
-	~MyVector();
+	delete[] data;
 }
 
 //doubles capacity if size exceeds capacity
 template<typename T>
-void MyVector<T>::CheckCap()
+void MyVector<T>::CheckCap() //Functions should only do one thing, so should have one function for checking cap and one for increasing cap, or just check cap in the function where you call increaseSize
 {
 	if (size >= capacity)
 	{
@@ -136,7 +132,7 @@ void MyVector<T>::CheckCap()
 
 //Return the size of the vector
 template<typename T>
-int MyVector<T>::GetSize()
+const int MyVector<T>::GetSize()
 {
 	return size;
 }
